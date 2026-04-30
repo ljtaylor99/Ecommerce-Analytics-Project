ff7f3ca56853ef1882ae2b718fb4a13d88dbc4ee
Lauren Taylor <laurenjtaylor99@gmail.com>
2026-04-30 20:50:48 +0000

    Change distribution center geo data and modify duplication filtering

--- transaction-data.sql
+++ transaction-data.sql
@@ -1,9 +1,18 @@
 /* Query will return transaction info about the product odered, geographic info from user, and when it was shipped and delivered. 
 Scheduled to run every 24 hours to extract data*/
 
-SELECT DISTINCT OI.id,order_id, category, P.name AS product_name, brand, department, ROUND(retail_price,2) AS retail_price,
-DC.name AS distribution_center,state,city,country,OI.created_at,shipped_at, delivered_at, returned_at, DC.latitude as DC_long, DC.longitude
-AS DC_lat,
+
+#Duplicate rows
+WITH duplicates AS (
+SELECT id, COUNT(*)
+FROM `bigquery-public-data.thelook_ecommerce.order_items` AS OI
+GROUP BY id, order_id
+HAVING COUNT(*) > 1
+)
+
+
+SELECT OI.id,order_id, category, P.name AS product_name, brand, department, ROUND(retail_price,2) AS retail_price,
+DC.name AS distribution_center,state,city,country,OI.created_at,shipped_at, delivered_at, returned_at, DC.distribution_center_geom AS DC_geom,
 # Calculate how many days it takes for product to arrive
 DATE_DIFF(delivered_at, shipped_at, DAY) AS delivery_time_days,
 # Put status as incomplete if delivery hasn't been made within 2 months 
@@ -20,6 +29,6 @@ ON P.distribution_center_id = DC.id
 # Get customer info
 LEFT JOIN `bigquery-public-data.thelook_ecommerce.users` AS U
 ON OI.user_id = U.id
-# Excludes null values in demographic and product info
-WHERE CONCAT(OI.id,OI.order_id,category,P.name,brand,department,retail_price,DC.name,state,city,country) IS NOT NULL
+# Excludes duplicates null values in demographic and product info
+WHERE (OI.id) NOT IN (SELECT id FROM duplicates) AND CONCAT(OI.id,OI.order_id,category,P.name,brand,department,retail_price,DC.name,state,city,country) IS NOT NULL 
 ORDER BY OI.created_at ASC
